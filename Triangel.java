37fe659dcfa34ebc5e8898f48889f09e9ef4c6d5
vidarsch <55668336+vidarsch@users.noreply.github.com>
2020-11-24 15:47:54 +0100

    Add files via upload

--- Triangel.java
+++ Triangel.java
@@ -17,6 +17,7 @@ public class Triangel {
 		double b = 0;
 		double c = 0;
 		double bis;
+		double p;
 			
 		if (selection == "110") {
 			 b = oneSide; 
@@ -35,11 +36,11 @@ public class Triangel {
 		
 		if (b>=c) {
 			double alfa = Math.acos(c/b);
-			double p = 2 * b * c * Math.cos (alfa / 2);
+			p = 2 * b * c * Math.cos (alfa / 2);
 			
 		} else {
 			double alfa = Math.acos(b/c);
-			double p = 2 * b * c * Math.cos (alfa / 2);
+			p = 2 * b * c * Math.cos (alfa / 2);
 			
 		}
 	bis = p / (b + c);
@@ -48,17 +49,36 @@ public class Triangel {
 			
 	}
 		
-	public double omkrets() {
-		return oneSide + twoSide + threeSide;
+	public static double omkrets(double oA, double oB, double oC) {
+		return oA+oB+oC;
 			
-			
+	}
+	
+	public double height() {
+		double Aa = oneSide;
+		double Bb = twoSide;
+		double Cc = threeSide;
+		double Length = 0;
+		
+		if (Aa>Bb && Aa>Cc) { 
+			Length = bisektris("011");
+		}
+		if (Bb>Cc && Bb>Aa) { 
+			Length = bisektris("101");
+		}
+		if (Cc>Bb && Cc>Aa) { 
+			Length = bisektris("110");
+		}
+		return Length;
 	}
 	
 	public static void main(String[] args) {
 			
-		Triangel myTriangle = new Triangel(2,4,2);
-		System.out.println(myTriangle.omkrets());
-		System.out.println(myTriangle.bisektris("110"));
+		Triangel myTriangle = new Triangel(4,3,2);
+		System.out.println("Omkretsen är :"+ myTriangle.omkrets(myTriangle.oneSide,myTriangle.twoSide,myTriangle.threeSide));
+		System.out.println("Sidorna på triangeln är :" + myTriangle.oneSide + ", " + myTriangle.twoSide + ", " + myTriangle.threeSide);
+		System.out.println("Bisektrisen mellan sidan A och B är : " + myTriangle.bisektris("110"));
+		System.out.println("height is : " + myTriangle.height());
 			
 	}
 	
